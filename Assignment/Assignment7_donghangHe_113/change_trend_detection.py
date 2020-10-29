633a46e229adea5a7bc33a0e15855e6f9da3f8c2
donghanghe <hdhssl1015>
2020-10-29 14:24:26 -0400

    week 7 update

--- Assignment/Assignment7_donghangHe_113/change_trend_detection.py
+++ Assignment/Assignment7_donghangHe_113/change_trend_detection.py
@@ -5,6 +5,7 @@ Created on Tue Oct 27 2019
 """
 
 import os
+import sys
 import pandas as pd
 import numpy as np
 import traceback
@@ -13,54 +14,45 @@ from scipy.stats import f as fisher_f
 
 input_dir = r'datasets'
 input_file = os.path.join(input_dir, 'HSBC_label.csv')
+# create linear regression model
+model = LinearRegression(fit_intercept=True)
 
 
 def candidate(df):
-    k_list = []
-    sse = []
+    # break day
+    best_k = 0
+    # minimize sse
+    sse = sys.maxsize
     length = len(df)
+    # sse left and right
+    l1, l2 = 0, 0
+    # find k
     for k in range(2, length):
-        x1 = np.array(range(1, k + 1)).reshape(-1, 1)
-        y1 = np.array(df['Close'][0:k])
+        # save sse left and right
+        l_list = []
+        # predict left and right
+        for i in range(2):
+            if i == 0:
+                start = 1
+                end = k + 1
+            else:
+                start = k + 1
+                end = length + 1
+            l_list.append(linear_model(start, end, df))
+        # whether is smaller than current sse
+        if sum(l_list) < sse:
+            best_k = k
+            sse = sum(l_list)
+            l1 = l_list[0]
+            l2 = l_list[1]
 
-        x2 = np.array(range(k+1, length + 1)).reshape((-1, 1))
-        y2 = np.array(df['Close'][k:])
-
-        model = LinearRegression(fit_intercept=True)
-        model.fit(x1, y1)
-        sse_l = np.sum((y1 - model.predict(x1)) ** 2)
-
-        model.fit(x2, y2)
-        sse_r = np.sum((y2 - model.predict(x2)) ** 2)
-
-        k_list.append(k)
-        sse.append(sse_l + sse_r)
-
-    best_k = k_list[sse.index(min(sse))]
-
-    x = np.array(range(1, length + 1)).reshape(-1, 1)
-    y = np.array(df['Close'])
-    x1 = np.array(range(1, best_k + 1)).reshape(-1, 1)
-    y1 = np.array(df['Close'][0:best_k])
-
-    x2 = np.array(range(best_k + 1, length + 1)).reshape((-1, 1))
-    y2 = np.array(df['Close'][best_k:])
-
-    model = LinearRegression(fit_intercept=True)
-
-    model.fit(x, y)
-    l = np.sum((y - model.predict(x)) ** 2)
-
-    model.fit(x1, y1)
-    l1 = np.sum((y1 - model.predict(x1)) ** 2)
-
-    model.fit(x2, y2)
-    l2 = np.sum((y2 - model.predict(x2)) ** 2)
-
+    # sse for whole month
+    l = linear_model(1, length + 1, df)
+    # f statistics
     f = ((l - (l1 + l2)) / 2) / ((l1 + l2) / (length - 4))
-
+    # f distribution
     p_value = fisher_f.cdf(f, 2, length - 4)
-    print(best_k)
+    # check significant change
     if p_value > 0.1:
         status = 'Yes'
     else:
@@ -69,16 +61,29 @@ def candidate(df):
     return status
 
 
+def linear_model(start, end, df):
+    # set x and y
+    x = np.array(range(start, end)).reshape(-1, 1)
+    y = np.array(df['Adj Close'][start - 1:end - 1])
+    # fit model
+    model.fit(x, y)
+    # predict and compute the sse
+    return np.sum((y - model.predict(x)) ** 2)
+
+
 def main():
     try:
+        # read file
         df = pd.read_csv(input_file)
-
+        # 2 years
         for i in range(2018, 2020):
             print("Year " + str(i))
             df_year = df[(df['Year']) == i]
+            # 12 months
             for j in range(1, 13):
                 df_month = df_year[df_year['Month'] == j]
                 status = candidate(df_month)
+                # print the trend
                 print("month: " + str(j) + " trend: " + str(status))
 
     except Exception as e:
