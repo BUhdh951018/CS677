3b86d6c44982e3297519dada9d8e920e87c93c4b
Donghang He <donghang@bu.edu>
2020-12-07 01:12:14 -0500

    project update

--- Assignment/CS677Project/compare.py
+++ Assignment/CS677Project/compare.py
@@ -5,146 +5,111 @@ Created on 2020/11/17 1:43 上午
 @FileName: compare.py
 @Software: PyCharm
 """
+import os
 import traceback
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+from method import output_dir, pd
 # import method and list
-from method import buy_and_hold, label_price, acc, week_number
+# from method import acc
 # import panda frame
-from method import df_new, x_train, y_train, x_test, x_train_sc, x_test_sc
+from method import df_corr, title_list, method
+# import training set
+from method import x_train, x_train_sc, y_train
+# import test set
+from method import x_test, x_test_sc
+# import accuracy function
+from method import acc
 # import model
 from method import svm_classifier, knn_classifier, logistic_classifier, nb_classifier, clf, model
 from method import lda_classifier, qda_classifier
 
-
-# todo 哪些需要sc
-def knn():
-    knn_classifier.fit(x_train_sc, y_train)
-    predicted = knn_classifier.predict(np.asmatrix(x_test_sc))
-    acc(predicted, 'kNN')
-    return predicted
+# show all row
+pd.options.display.max_columns = None
+pd.options.display.max_rows = None
+np.set_printoptions(threshold=np.inf)
 
 
-def logistic():
-    logistic_classifier.fit(x_train, y_train)
-    predicted = logistic_classifier.predict(np.asmatrix(x_test))
-    acc(predicted, 'logistic regression')
-    return predicted
+def knn(i):
+    knn_classifier.fit(x_train_sc[i], y_train[i])
+    predicted = knn_classifier.predict(np.asmatrix(x_test_sc[i]))
+    return acc(predicted, 'kNN', i)
 
 
-def naive():
-    nb_classifier.fit(x_train, y_train)
-    predicted = nb_classifier.predict(np.asmatrix(x_test))
-    acc(predicted, 'naive bayesian')
-    return predicted
+def logistic(i):
+    logistic_classifier.fit(x_train[i], y_train[i])
+    predicted = logistic_classifier.predict(np.asmatrix(x_test[i]))
+    return acc(predicted, 'logistic regression', i)
 
 
-def tree():
-    clf.fit(x_train, y_train)
-    predicted = clf.predict(np.asmatrix(x_test))
-    acc(predicted, 'decision tree')
-    return predicted
+def naive(i):
+    nb_classifier.fit(x_train[i], y_train[i])
+    predicted = nb_classifier.predict(np.asmatrix(x_test[i]))
+    return acc(predicted, 'naive bayesian', i)
 
 
-def forest():
-    model.fit(x_train, y_train)
-    predicted = model.predict(np.asmatrix(x_test))
-    acc(predicted, 'random forest')
-    return predicted
+def tree(i):
+    clf.fit(x_train[i], y_train[i])
+    predicted = clf.predict(np.asmatrix(x_test[i]))
+    return acc(predicted, 'decision tree', i)
 
 
-def lda():
-    lda_classifier.fit(x_train_sc, y_train)
-    predicted = lda_classifier.predict(np.asmatrix(x_test_sc))
-    acc(predicted, 'LDA')
-    return predicted
+def forest(i):
+    model.fit(x_train[i], y_train[i])
+    predicted = model.predict(np.asmatrix(x_test[i]))
+    return acc(predicted, 'random forest', i)
 
 
-def qda():
-    qda_classifier.fit(x_train_sc, y_train)
-    predicted = qda_classifier.predict(np.asmatrix(x_test_sc))
-    acc(predicted, 'QDA')
-    return predicted
+def lda(i):
+    lda_classifier.fit(x_train_sc[i], y_train[i])
+    predicted = lda_classifier.predict(np.asmatrix(x_test_sc[i]))
+    return acc(predicted, 'LDA', i)
 
 
-def svm():
-    svm_classifier.fit(x_train_sc, y_train)
-    predicted = svm_classifier.predict(np.asmatrix(x_test_sc))
-    acc(predicted, 'SVM')
-    return predicted
+def qda(i):
+    qda_classifier.fit(x_train_sc[i], y_train[i])
+    predicted = qda_classifier.predict(np.asmatrix(x_test_sc[i]))
+    return acc(predicted, 'QDA', i)
+
+
+def svm(i):
+    svm_classifier.fit(x_train_sc[i], y_train[i])
+    predicted = svm_classifier.predict(np.asmatrix(x_test_sc[i]))
+    return acc(predicted, 'SVM', i)
+
+
+def visual_corr(df, title):
+    # correlation matrix
+    corr = df.corr()
+    # absolute the result
+    corr = corr.abs()
+    # show by the heat map
+    plt.title('correlation matrix heatmap of NBA players')
+    sns.heatmap(corr)
+    # save the plot as png file
+    plt.savefig(os.path.join(output_dir, 'correlation_matrix_nba_' + title + '.png'))
+    plt.show()
+
+
+def draw(accuracy, title):
+    plt.figure()
+    plt.plot(method, accuracy, color='blue', linestyle='dashed', marker='o', markerfacecolor='red', markersize=10)
+    plt.title('Accuracy of all model for ' + title + 'columns')
+    plt.xlabel('Prediction Model')
+    plt.ylabel('Accuracy')
+    plt.savefig(os.path.join(output_dir, 'accuracy_nba_' + title + '.png'))
+    plt.show()
 
 
 def main():
     try:
-        # buy and hold strategy
-        df_bh = df_new[['Open', 'Close', 'Week_Number']]
-        data = np.array(df_bh)
-        price = buy_and_hold(data.tolist())
-        print("final price for buy and hold strategy", round(price[-1], 4))
-        print()
-        plt.plot(week_number, price, color='tab:blue', label='buy-and-hold')
-
-        # knn strategy k=7
-        predict_knn = knn()
-        price = label_price(data, predict_knn)
-        print("final price for KNN strategy", round(price[-1], 4))
-        print()
-        plt.plot(week_number, price, color='tab:orange', label='kNN')
-
-        # logistic regression
-        predict_lr = logistic()
-        price = label_price(data, predict_lr)
-        print("final price for logistic regression strategy", round(price[-1], 4))
-        print()
-        plt.plot(week_number, price, color='tab:green', label='logistic')
-
-        # naive bayesian
-        predict_nb = naive()
-        price = label_price(data, predict_nb)
-        print("final price for naive bayesian strategy", round(price[-1], 4))
-        print()
-        plt.plot(week_number, price, color='tab:red', label='naive bayesian')
-
-        # decision tree
-        predict_dt = tree()
-        price = label_price(data, predict_dt)
-        print("final price for decision tree strategy", round(price[-1], 4))
-        print()
-        plt.plot(week_number, price, color='tab:purple', label='decision tree')
-
-        # random forest
-        predict_rf = forest()
-        price = label_price(data, predict_rf)
-        print("final price for random forest strategy", round(price[-1], 4))
-        print()
-        plt.plot(week_number, price, color='tab:brown', label='random forest')
-
-        # linear discriminant
-        predict_ld = lda()
-        price = label_price(data, predict_ld)
-        print("final price for linear discriminant strategy", round(price[-1], 4))
-        print()
-        plt.plot(week_number, price, color='tab:pink', label='linear discriminant')
-
-        # quadratic discriminant
-        predict_qd = qda()
-        price = label_price(data, predict_qd)
-        print("final price for quadratic discriminant strategy", round(price[-1], 4))
-        print()
-        plt.plot(week_number, price, color='tab:olive', label='quadratic discriminant')
-
-        # svm strategy
-        predict_svm = svm()
-        price = label_price(data, predict_svm)
-        print("final price for SVM strategy", round(price[-1], 4))
-        print()
-        plt.plot(week_number, price, color='tab:gray', label='linear SVM')
-
-        plt.title('portfolio growth')
-        plt.xlabel('week number')
-        plt.ylabel('portfolio value')
-        plt.legend()
-        plt.show()
+        # show the correlation heat map
+        # for i in range(2):
+        #     visual_corr(df_corr[i], title_list[i])
+        for i in range(2):
+            accuracy = [knn(i), logistic(i), naive(i), tree(i), forest(i), lda(i), qda(i), svm(i)]
+            draw(accuracy, title_list[i])
 
     except Exception as e:
         print(e)
